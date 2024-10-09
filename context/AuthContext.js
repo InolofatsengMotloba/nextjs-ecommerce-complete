5c1519cc74ea51ece30fbeee6e91b0b90fc95ddc
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-10-10 00:32:51 +0200

    Manage and display the authentication state by creating a context for managing the authentication state across the app

---
+++ context/AuthContext.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/config/firebaseConfig";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
