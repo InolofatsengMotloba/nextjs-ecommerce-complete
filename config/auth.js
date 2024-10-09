5ed02b5c69a43c80a36fce766ff3ffff1d60caac
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-10-10 00:21:56 +0200

    Create Authentication Functions: Implement the sign-up, sign-in, sign-out functionality using Firebase Authentication

---
+++ config/auth.js
@@ -0,0 +1,40 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebaseConfig";
+
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signOutUser = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    throw error;
+  }
+};
